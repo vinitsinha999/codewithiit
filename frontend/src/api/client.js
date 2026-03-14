c0b45ed6519b54e8dd3748739a69ed2928441f42
Vinit Kumar <vinitkumargaya.vk@gmail.com>
2026-03-14 17:37:56 +0530

    Fix API base URL to use env variable

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -11,7 +11,7 @@
 
 import axios from 'axios'
 
-const API_BASE = 'http://localhost:8000'
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
 const apiClient = axios.create({
   baseURL: API_BASE,
